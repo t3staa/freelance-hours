8f5e26991ee00cf06808127fa000aee2f0aac617
t3staa <testa.leo@outlook.com>
2024-10-09 08:36:55 -0300

    criacao dos models user e project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Project;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->count(200)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+        ->each(fn (User $u) => Project::factory()->count(10)->create(['created_by' => $u->id]));
+    }
+}
