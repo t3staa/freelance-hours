3c3faf75fb85d22358bfc06cf3cd2a64ce93b168
t3staa <testa.leo@outlook.com>
2024-10-09 10:54:26 -0300

    criando livewire components

---
+++ resources/views/livewire/projects/show.blade.php
@@ -0,0 +1,7 @@
+<div>
+    componente livewire projects.show
+    <pre>
+        title: {{ $project->titele }}
+        description: {!! $project->description !!}
+    </pre>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProjectsController;
 
-Route::get('/', WelcomeController::class);
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
+Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
