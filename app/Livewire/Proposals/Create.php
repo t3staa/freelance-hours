87a38805790436e94a20a6f846b885ca5ae08d8a
t3staa <testa.leo@outlook.com>
2024-10-10 20:56:33 -0300

    atualizando posicoes

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -36,17 +36,41 @@ class Create extends Component
             return;
         }
 
-
-        $this->project->proposals()->updateOrCreate(
-            ['email' => $this->email],
-            ['hours' => $this->hours]
-        );
+        DB::transaction(function(){
+            $proposal = $this->project->proposals()->updateOrCreate(
+                ['email' => $this->email],
+                ['hours' => $this->hours]
+            );
+    
+            $this->arrangePositions($proposal);
+        });
+        
 
         $this->dispatch('proposal::created');
 
         $this->modal = false;
 
     }
+
+    public function arrangePositions(Proposal $proposal){
+        $query = DB::select("
+            select *, row_number() over (order by hours asc) as newPosition 
+            from proposals
+            where project_id = :project
+        ", ['project' => $proposal->project_id]);
+
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+
+        if($otherProposal){
+            $proposal->update(['position_status' => 'up']);
+            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+        }
+
+        ArrangePositions::run($proposal->project_id);
+    }
+
     public function render()
     {
         return view('livewire.proposals.create');
